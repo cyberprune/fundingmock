07b530dc5d8c537e202d6deb76396bcaf70e9541
Ryan Barlow <ryan-barlow@users.noreply.github.com>
2019-06-25 15:42:46 +0100

    Tidy up

--- FundingMock.Web/Models/Providers/ProviderAddress.cs
+++ FundingMock.Web/Models/Providers/ProviderAddress.cs
@@ -1,13 +1,9 @@
-﻿using Newtonsoft.Json;
-
-namespace FundingMock.Web.Models.Providers
+﻿namespace FundingMock.Web.Models.Providers
 {
     public class ProviderAddress
     {
-        [JsonProperty("town")]
         public string Town { get; set; }
 
-        [JsonProperty("postcode")]
         public string Postcode { get; set; }
     }
 }
